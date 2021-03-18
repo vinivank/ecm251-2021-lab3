0a7e76d1f4ae5f8038ce22abaf383e78cfbde9ef
vinivank <vini.ivankovich@users.noreply.github.com>
2021-03-18 09:26:50 -0300

    testes

--- aula5teo/src/ivankovich/vinicius/Main.java
+++ aula5teo/src/ivankovich/vinicius/Main.java
@@ -3,7 +3,7 @@ package ivankovich.vinicius;
 public class Main {
 
     public static void main(String[] args) {
-        String cpf = "499.365.28875";
+        String cpf = "19672820801";
         System.out.println("Cpf valido:" + ValidadorCPF.validarCpf(cpf));
     }
 }
