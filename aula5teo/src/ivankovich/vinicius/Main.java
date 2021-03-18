5081b4c1505267eb473822e4e515a5a6e2aaead0
vinivank <vini.ivankovich@users.noreply.github.com>
2021-03-18 09:15:23 -0300

    validador de cpf

---
+++ aula5teo/src/ivankovich/vinicius/Main.java
@@ -0,0 +1,9 @@
+package ivankovich.vinicius;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cpf = "499.365.28875";
+        System.out.println("Cpf valido:" + ValidadorCPF.validarCpf(cpf));
+    }
+}
