5e17e16c7fd38808e548a34930b19394acffaec3
vinivank <vini.ivankovich@users.noreply.github.com>
2021-04-05 13:49:57 -0300

    formatação e leitor de input de usuario

--- atividade_t1/src/ivankovich/vinicius/Main.java
+++ atividade_t1/src/ivankovich/vinicius/Main.java
@@ -1,23 +1,32 @@
 package ivankovich.vinicius;
 
 
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Conta c1=new Conta() ,c2=new Conta(),c3=new Conta() ;
         transacoes t=new transacoes();
-        c1.cliente.usuario= "microsoft";
-        c2.cliente.usuario= "samsung";
-        c3.cliente.usuario= "apple" ;
+        System.out.println("digite o primeiro usuario");
+        Scanner u1=new Scanner(System.in);
+
+        c1.cliente.usuario= u1.nextLine();
+        System.out.println("digite o segundo usuario");
+        c2.cliente.usuario= u1.nextLine();
+        System.out.println("digite o terceiro usuario");
+        c3.cliente.usuario= u1.nextLine() ;
         c1.saldo=1000;
         c2.saldo=250;
         c3.saldo=3000;
 
 
         System.out.println("saldos antes");
+        System.out.println("saldo de "+c1.cliente.usuario+":  ");
         System.out.println(c1.saldo);
+        System.out.println("saldo de "+c2.cliente.usuario+":  ");
         System.out.println(c2.saldo);
+        System.out.println("saldo de "+c3.cliente.usuario+":  ");
         System.out.println(c3.saldo);
 
 
@@ -33,8 +42,11 @@ public class Main {
 
 
         System.out.println("saldos depois");
+        System.out.println("saldo de "+c1.cliente.usuario+":  ");
         System.out.println(c1.saldo);
+        System.out.println("saldo de "+c2.cliente.usuario+":  ");
         System.out.println(c2.saldo);
+        System.out.println("saldo de "+c3.cliente.usuario+":  ");
         System.out.println(c3.saldo);
 
 
