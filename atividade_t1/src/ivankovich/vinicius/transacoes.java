9b0801bec4a05228137afcf2bc5dc593148703f4
vinivank <vini.ivankovich@users.noreply.github.com>
2021-04-04 22:11:28 -0300

    inicio da operação de pagamento

--- atividade_t1/src/ivankovich/vinicius/transacoes.java
+++ atividade_t1/src/ivankovich/vinicius/transacoes.java
@@ -12,10 +12,17 @@ public class transacoes {
         int tid=getRandomNumberInRange(1000,9999);
         String qr;
         qr=conta.id+";"+cliente.usuario+";"+qua+";"+tid  ;
-        return qr
+        return qr;
 
 
 
+
+
+    }
+
+    public void pagar(String qr){
+        String[] dados = qr.split( ";" ) ;
+        conta.transferirPara(dados[2],dados[0]);
 
 
     }
