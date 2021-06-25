e5f378b0c4ef49e44c0ff9b5fb3057deba4eafd9
vinivank <vini.ivankovich@users.noreply.github.com>
2021-06-25 16:16:18 -0300

    começo do adicionar membros e loop de execução

--- atividade_t2/src/ivankovich/vinicius/models/Sistema.java
+++ atividade_t2/src/ivankovich/vinicius/models/Sistema.java
@@ -6,11 +6,23 @@ import java.util.Scanner;
 
 public class Sistema {
 
-    ArrayList<Membro> bancoMembros = new ArrayList<>();
+    ArrayList<Membro> dadosMembros = new ArrayList<>();
+    private boolean exec = true;
+
+    Scanner acao = new Scanner(System.in);
+    Scanner usuarios = new Scanner(System.in);
+    Scanner emails = new Scanner(System.in);
+    Scanner funcaos = new Scanner(System.in);
 
 
     public void Executar(){
-        Menu();
+
+        int opcao;
+        while (exec){
+            Menu();
+            opcao = acao.nextInt();
+            avaliarOpcao(opcao);
+        }
 
 
 
@@ -24,6 +36,47 @@ public class Sistema {
         System.out.println("[ 1 ] Cadastrar membro");
         System.out.println("[ 2 ] Remover membro");
         System.out.println("[ 3 ] Exibir horário");
+        System.out.println("[ 4 ] Sair \n");
     }
+
+
+
+
+    private void avaliarOpcao(int opcao) {
+        switch (opcao){
+            case 0:
+                System.out.println("Até Logo!");
+                this.exec = false;
+                break;
+            case 1:
+                System.out.println("Digite o usuario: ");
+                String usuario = usuarios.next();
+                System.out.println("Digite o email : ");
+                String email = emails.next();
+                System.out.println("Escolha a função:\n [ 1 ] MobileMember\n [ 2 ] HeavyLifter\n [ 3 ] ScriptGuys\n [ 4 ] BigBrothers ");
+                int funcao = funcaos.nextInt();
+
+                break;
+            case 2:
+
+
+
+                break;
+            case 3:
+
+
+
+                break;
+            case 4:
+                exec=false;
+                break;
+            default:
+                System.out.println("Opcao invalida");
+                break;
+        }
+    }
+
+
+
 
 }
