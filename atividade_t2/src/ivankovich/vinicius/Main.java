41a24f93a77fa076abd544c1b741bb6abeef44ff
vinivank <vini.ivankovich@users.noreply.github.com>
2021-06-25 15:46:50 -0300

    execução do sistema adicionada á main

--- atividade_t2/src/ivankovich/vinicius/Main.java
+++ atividade_t2/src/ivankovich/vinicius/Main.java
@@ -1,10 +1,13 @@
 package ivankovich.vinicius;
 
+import ivankovich.vinicius.models.Sistema;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        Sistema system =new Sistema();
+        system.Executar();
 
 
     }
